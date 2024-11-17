1de106e2453c10a6edb710ee02c9bdf7f06186c1
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2024-11-17 14:39:18 +0600

    pop up modal with items details added

--- js/fetch.js
+++ js/fetch.js
@@ -8,11 +8,11 @@ const fetched = async() =>{
 
 const gadgets = ai =>{
     //console.log(ai);
-    for (item of ai){
-        console.log(item);
+    ai.forEach(item => {
+        //console.log(item);
         const cardElement = document.getElementById('card-container');
         const cardItem = document.createElement('div');
-        cardItem.classList = `card card-compact bg-grey-100 p-4 shadow-xl`;
+        cardItem.classList = `card card-compact bg-grey-100 w-96 p-4 shadow-xl rounded-lg`;
         cardItem.innerHTML = `
                     <figure>
                         <img
@@ -22,19 +22,60 @@ const gadgets = ai =>{
                     <div class="card-body">
                         <h2 class="card-title">Features</h2>
                         <ul class="mb-3">
-                        <li>${item.features[0]}</li>
-                        <li>${item.features[1]}</li>
-                        <li>${item.features[2]}</li>
+                            <li>${item.features[0]}</li>
+                            <li>${item.features[1]}</li>
+                            <li>${item.features[2]}</li>
                         </ul>
                         <hr>
                         <p>${item.name}</p>
                         <p>${item.published_in}</p>
-                        </div>
+                        <button onClick = "handleShowDetail('${item.id}')" class="btn btn-primary">Show More Details <button/>
                     </div>`     
-              cardElement.appendChild(cardItem);      
+              cardElement.appendChild(cardItem);   
+    });
+            
     }
-}
 
+const handleShowDetail = async (id) => {
+    //console.log('clicked show detail',id);
+
+    const res = await fetch(`https://openapi.programming-hero.com/api/ai/tool/${id}`);
+    const data = await res.json();
+    //console.log(data);
+    const phone = data.data;
+    console.log(phone);
+    showPhoneDetails(phone);
+}   
+
+const showPhoneDetails = (phone) =>{
+    console.log(phone);
+
+    const showDetailPhoneName = document.getElementById('show_detail_phone_name');
+    showDetailPhoneName.innerText = phone.tool_name;
+
+    const showDetailContainer = document.getElementById('show_detail_container');
+    showDetailContainer.innerHTML = `
+    <div class= "flex items-center justify-center gap-10">
+        <div class="space-y-2">
+            <h3 class="text-2xl">${phone.description}</h3>
+            <div class="flex items-center justify-between gap-5">
+                <div><span>${phone.pricing[0].price}</span><br><span>${phone.pricing[0].plan}</span></div>
+                <div><span>${phone.pricing[1].price}</span><br><span>${phone.pricing[1].plan}</span></div>
+                <div><span>${phone.pricing[2].price}</span><br><span>${phone.pricing[2].plan}</span></div>
+            </div>
+        </div>    
+        <div>
+            <figure>
+                <img
+                src="${phone.image_link[0]}"
+                class="max-w-full"
+                alt="Shoes" />
+            </figure>
+        </div>
+    </div>
+    `;
+    showDetails_modal.showModal();
+}
 
 
 fetched();

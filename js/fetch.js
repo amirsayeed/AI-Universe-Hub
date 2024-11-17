628ced43de612ff0eebc58f5ce6c52aae806be9f
amirsayeed <68963306+amirsayeed@users.noreply.github.com>
2024-11-17 22:32:55 +0600

    details modal completed

--- js/fetch.js
+++ js/fetch.js
@@ -29,7 +29,7 @@ const gadgets = ai =>{
                         <hr>
                         <p>${item.name}</p>
                         <p>${item.published_in}</p>
-                        <button onClick = "handleShowDetail('${item.id}')" class="btn btn-primary">Show More Details <button/>
+                        <button onClick = "handleShowDetail('${item.id}')" class="btn btn-error rounded-lg">Show More Details <button/>
                     </div>`     
               cardElement.appendChild(cardItem);   
     });
@@ -50,27 +50,45 @@ const handleShowDetail = async (id) => {
 const showPhoneDetails = (phone) =>{
     console.log(phone);
 
-    const showDetailPhoneName = document.getElementById('show_detail_phone_name');
-    showDetailPhoneName.innerText = phone.tool_name;
-
     const showDetailContainer = document.getElementById('show_detail_container');
     showDetailContainer.innerHTML = `
-    <div class= "flex items-center justify-center gap-10">
-        <div class="space-y-2">
-            <h3 class="text-2xl">${phone.description}</h3>
-            <div class="flex items-center justify-between gap-5">
+    <div class= "flex items-center justify-center gap-10 p-5">
+        <div class="space-y-6 rounded-lg shadow-xl p-8">
+            <h3 class="text-2xl font-bold">${phone.description}</h3>
+            <div class="flex items-center justify-around gap-10">
                 <div><span>${phone.pricing[0].price}</span><br><span>${phone.pricing[0].plan}</span></div>
                 <div><span>${phone.pricing[1].price}</span><br><span>${phone.pricing[1].plan}</span></div>
                 <div><span>${phone.pricing[2].price}</span><br><span>${phone.pricing[2].plan}</span></div>
             </div>
+
+            <div class= "flex items-center justify-between gap-4">
+                <div class='space-y-2'>
+                 <h3 class="text-2xl font-bold">Features</h3>
+                 <ul>
+                    <li>${phone.features[1].feature_name}</li>
+                    <li>${phone.features[2].feature_name}</li>
+                    <li>${phone.features[3].feature_name}</li>
+                 </ul>
+                </div>
+                <div class='space-y-2'>
+                    <h3 class="text-2xl font-bold">Integrations</h3>
+                    <ul>
+                        <li>${phone.integrations[0]}</li>
+                        <li>${phone.integrations[1]}</li>
+                        <li>${phone.integrations[2]}</li>
+                    </ul>
+                </div>
+            </div>
         </div>    
-        <div>
+        <div class='space-y-3'>
             <figure>
                 <img
                 src="${phone.image_link[0]}"
                 class="max-w-full"
                 alt="Shoes" />
             </figure>
+            <p class='text-center font-semibold text-xl'>${phone.input_output_examples[0].input}</p>
+            <p class='text-center'>${phone.input_output_examples[0].output}</p>
         </div>
     </div>
     `;
